a0fd73738356880891103c3ed5c9c93e8b7e7c10
Muhammad Raahim Khan <21100157@lums.edu.pk>
2020-07-22 18:57:56 +0500

    added api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react' ;
 import AllNews from './News'
 import AddNew from './AddNews'
+import axios from 'axios';
 
 class App extends Component {
   state = {
@@ -20,19 +21,19 @@ class App extends Component {
   }
 
   addNews = (neWs) => {
-    // http://127.0.0.1:5000/
 
-    // API handling
-
-    // API handling ends
-
-    neWs.id = Math.random() ;
-    neWs.truth = "True" ;
-    neWs.content = neWs.content + ' :' ;
-    let news = [...this.state.news, neWs] ;
-    this.setState({
-      news 
-    })
+    axios.post('https://faker-detector.herokuapp.com/', {'text': neWs.content})
+            .then((res) => {
+              neWs.id = Math.random() ;
+              neWs.content = neWs.content + ' :' ;
+              neWs.truth = res.data ;
+              let news = [...this.state.news, neWs] ;
+              this.setState({
+                news 
+              })
+            }).catch((error) => {
+                alert("Server is busy, please try again later!")
+            });
   }
 
   render () {
@@ -41,7 +42,7 @@ class App extends Component {
         <h1>Fake News Detector</h1>
         <AddNew addNews={this.addNews}/>
         <AllNews allNews={ this.state.news } deleteNews={ this.deleteNews }/>
-        <div class="footer">
+        <div className="footer">
           <p>Bay Area Hacks</p>
         </div>
       </div>
